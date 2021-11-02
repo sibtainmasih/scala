8e223bd08f724d990797345dcee83864bb80160e
sibtainmasih <sibtain.masih@gmail.com>
2021-11-02 06:55:40 +0530

    Synchronous with sub observable and io scheduler

--- reactive/DishWasher.scala
+++ reactive/DishWasher.scala
@@ -2,6 +2,7 @@ import monix.eval.Task
 import monix.reactive.Observable
 import monix.execution.Ack
 import monix.execution.Scheduler
+import monix.execution.ExecutionModel.AlwaysAsyncExecution
 import scala.concurrent.duration._
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
@@ -35,7 +36,9 @@ val stream = Observable
   .flatMap(d =>
     Observable
       .fromTask(d.wash)
-      .executeOn(Scheduler.computation(name = "sub-comp-ctx", parallelism = 3))
+      .executeOn(
+        Scheduler.io(name = "sub-io-ctx", executionModel = AlwaysAsyncExecution)
+      )
   )
   .doOnNext { d =>
     Task {
@@ -55,38 +58,38 @@ Thread.sleep(12000)
 println(s"$log Program Ends.")
 
 /* Output
-[01:19:14.7680] [run-main-15] Program starts.
-[01:19:14.7780] [run-main-15] E: 1
-[01:19:14.7840] [sub-comp-ctx-1248] washing dish 1
-[01:19:15.7920] [sub-comp-ctx-1248] Dish washed 1.
-[01:19:15.7930] [sub-comp-ctx-1248] O:Dish(1,Clean)
-[01:19:15.8040] [main-comp-ctx-1249] E: 2
-[01:19:15.8060] [sub-comp-ctx-1251] washing dish 2
-[01:19:16.8060] [sub-comp-ctx-1251] Dish washed 2.
-[01:19:16.8070] [sub-comp-ctx-1251] O:Dish(2,Clean)
-[01:19:16.8080] [main-comp-ctx-1249] E: 3
-[01:19:16.8100] [sub-comp-ctx-1252] washing dish 3
-[01:19:17.8110] [sub-comp-ctx-1252] Dish washed 3.
-[01:19:17.8110] [sub-comp-ctx-1252] O:Dish(3,Clean)
-[01:19:17.8120] [main-comp-ctx-1249] E: 4
-[01:19:17.8150] [sub-comp-ctx-1253] washing dish 4
-[01:19:18.8150] [sub-comp-ctx-1253] Dish washed 4.
-[01:19:18.8150] [sub-comp-ctx-1253] O:Dish(4,Clean)
-[01:19:18.8160] [main-comp-ctx-1249] E: 5
-[01:19:18.8190] [sub-comp-ctx-1254] washing dish 5
-[01:19:19.8200] [sub-comp-ctx-1254] Dish washed 5.
-[01:19:19.8200] [sub-comp-ctx-1254] O:Dish(5,Clean)
-[01:19:19.8220] [main-comp-ctx-1249] E: 6
-[01:19:19.8240] [sub-comp-ctx-1255] washing dish 6
-[01:19:20.8250] [sub-comp-ctx-1255] Dish washed 6.
-[01:19:20.8260] [sub-comp-ctx-1255] O:Dish(6,Clean)
-[01:19:20.8270] [main-comp-ctx-1249] E: 7
-[01:19:20.8290] [sub-comp-ctx-1256] washing dish 7
-[01:19:21.8300] [sub-comp-ctx-1256] Dish washed 7.
-[01:19:21.8300] [sub-comp-ctx-1256] O:Dish(7,Clean)
-[01:19:21.8300] [main-comp-ctx-1249] E: 8
-[01:19:21.8320] [sub-comp-ctx-1257] washing dish 8
-[01:19:22.8330] [sub-comp-ctx-1257] Dish washed 8.
-[01:19:22.8340] [sub-comp-ctx-1257] O:Dish(8,Clean)
-[01:19:26.8810] [run-main-15] Program Ends.
+[01:24:56.7340] [run-main-17] Program starts.
+[01:24:56.7440] [run-main-17] E: 1
+[01:24:56.7520] [sub-io-ctx-1334] washing dish 1
+[01:24:57.7610] [sub-io-ctx-1334] Dish washed 1.
+[01:24:57.7620] [sub-io-ctx-1334] O:Dish(1,Clean)
+[01:24:57.7680] [main-comp-ctx-1335] E: 2
+[01:24:57.7720] [sub-io-ctx-1338] washing dish 2
+[01:24:58.7730] [sub-io-ctx-1338] Dish washed 2.
+[01:24:58.7730] [sub-io-ctx-1338] O:Dish(2,Clean)
+[01:24:58.7760] [main-comp-ctx-1335] E: 3
+[01:24:58.7810] [sub-io-ctx-1340] washing dish 3
+[01:24:59.7820] [sub-io-ctx-1340] Dish washed 3.
+[01:24:59.7820] [sub-io-ctx-1340] O:Dish(3,Clean)
+[01:24:59.7830] [main-comp-ctx-1335] E: 4
+[01:24:59.7880] [sub-io-ctx-1342] washing dish 4
+[01:25:00.7880] [sub-io-ctx-1342] Dish washed 4.
+[01:25:00.7890] [sub-io-ctx-1342] O:Dish(4,Clean)
+[01:25:00.7900] [main-comp-ctx-1335] E: 5
+[01:25:00.7930] [sub-io-ctx-1344] washing dish 5
+[01:25:01.7940] [sub-io-ctx-1344] Dish washed 5.
+[01:25:01.7950] [sub-io-ctx-1344] O:Dish(5,Clean)
+[01:25:01.7960] [main-comp-ctx-1335] E: 6
+[01:25:01.8000] [sub-io-ctx-1346] washing dish 6
+[01:25:02.8000] [sub-io-ctx-1346] Dish washed 6.
+[01:25:02.8000] [sub-io-ctx-1346] O:Dish(6,Clean)
+[01:25:02.8010] [main-comp-ctx-1335] E: 7
+[01:25:02.8050] [sub-io-ctx-1348] washing dish 7
+[01:25:03.8050] [sub-io-ctx-1348] Dish washed 7.
+[01:25:03.8050] [sub-io-ctx-1348] O:Dish(7,Clean)
+[01:25:03.8060] [main-comp-ctx-1335] E: 8
+[01:25:03.8100] [sub-io-ctx-1350] washing dish 8
+[01:25:04.8110] [sub-io-ctx-1350] Dish washed 8.
+[01:25:04.8110] [sub-io-ctx-1350] O:Dish(8,Clean)
+[01:25:08.7990] [run-main-17] Program Ends.
 */
